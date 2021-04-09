01f1c595ace00dd99ccde6b50a77a4fd7f20cd94
zhouxiaoyuan <zhouxiaoyuanyouxi@126.com>
2021-04-09 14:40:58 +0800

    zookeeper

--- zookeeper-module/src/main/java/NodesTest.java
+++ zookeeper-module/src/main/java/NodesTest.java
@@ -32,17 +32,51 @@ public class NodesTest {
     @Test
     public void test(){
 
-
         CallBackTest cb = new CallBackTest();
+
+        Stat stat = new Stat();
+        try{
+            byte[]  bs = zk.getData("/", cb, stat);
+            System.out.println( "bs----------->" + new String(bs) );
+        }catch (Exception e){
+            e.printStackTrace();;
+        }
+
+        Stat ss = new Stat();
+        try{
+            byte[] bb = zk.getData("/node3", false, ss);
+            System.out.println(bb);
+            zk.delete("/node3",-1);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+        zk.exists("", true, cb, "");
+
+//        zk.create("/lock","lock".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_SEQUENTIAL,cb,"abc");
+
         for(int i = 0 ; i < 100000 ; i++){
             try{
-                String s = "/" + i + "";
-                zk.create(s, s.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
-//                zk.create(s,s.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL,cb,s);
+                String s = "/node" + i + "";
+                Stat stat1 = new Stat();
+//                zk.create(s, s.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+//                zk.create(s,"node".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT,cb,"abc");
+                zk.getData(s, cb, stat1);
+                zk.delete(s,stat1.getVersion());
             }catch (Exception e){
                 e.printStackTrace();
             }
         }
+
+        while(true){
+            try {
+                Thread.sleep(1000);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
     }
 
 }
