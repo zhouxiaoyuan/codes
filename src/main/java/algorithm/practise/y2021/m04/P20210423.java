665bb24399851514f5a9e2de6a61c410003897c3
zhouxiaoyuan <zhouxiaoyuanyouxi@126.com>
2021-04-23 10:22:27 +0800

    笔记

---
+++ src/main/java/algorithm/practise/y2021/m04/P20210423.java
@@ -0,0 +1,107 @@
+package algorithm.practise.y2021.m04;
+
+import algorithm.practise.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * @Description algorithm.practise.y2021.m04.P20210423
+ * @Created by zhouxiaoyuan on 2021/4/23 9:27
+ */
+public class P20210423 {
+
+    public static void main(String[] args) {
+        TreeNode root = TreeNode.getTree(3, 50);
+        TreeNode node = root;
+        System.out.println("");
+        TreeNode.dg(node,1);
+        System.out.println("");
+//        level(root);
+        pre2(node);
+        System.out.println("");
+    }
+
+    public static void pre(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        while ( !stack.empty() || node != null) {
+            if (node != null) {
+                stack.push(node);
+                node = node.left;
+            } else {
+                node = stack.pop();
+                if( stack.empty() || node != stack.peek().right ){
+                    System.out.print(node.val + "-");
+                }
+                node = node.right;
+            }
+        }
+    }
+
+    public static void pre2(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        while (!stack.empty() || node != null) {
+            while(node != null ){
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop();
+            System.out.print(node.val + "-");
+            node = node.right;
+            /*if( !stack.empty() && node == stack.peek().left ){
+                node = stack.peek().right;
+            }else{
+                node = null;
+            }*/
+
+        }
+
+    }
+
+
+    public static void level(TreeNode node){
+        if(node==null){
+            return ;
+        }
+        Queue<TreeNode> queue = new LinkedList();
+        queue.offer(node);
+        while(!queue.isEmpty()){
+            node = queue.poll();
+            System.out.print(node.val + "-");
+            if( node.left != null){
+                queue.offer(node.left);
+            }
+            if( node.right != null){
+                queue.offer(node.right);
+            }
+        }
+    }
+
+
+    public static void pre1(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(node);
+        while (!stack.empty()) {
+
+            node = stack.pop();
+            System.out.print(node.val + "-");
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+    }
+
+}
