75a5a6457aebe4b915640b252ba4ab718050c5c0
zhouxiaoyuan <zhouxiaoyuanyouxi@126.com>
2021-04-29 09:52:38 +0800

    算法

--- src/main/java/algorithm/practise/y2021/m04/P20210429.java
+++ src/main/java/algorithm/practise/y2021/m04/P20210429.java
@@ -1,5 +1,6 @@
 package algorithm.practise.y2021.m04;
 
+import algorithm.practise.ArrayFactory;
 import algorithm.practise.TreeNode;
 
 import java.util.Stack;
@@ -21,7 +22,7 @@ public class P20210429 {
         }
         System.out.println();*/
 
-        TreeNode node = TreeNode.getTree(3, 50);
+        /*TreeNode node = TreeNode.getTree(3, 50);
         TreeNode root = node;
         StringBuilder s2 = new StringBuilder();
         String s1 = TreeNode.dg(node ,2 );
@@ -29,10 +30,57 @@ public class P20210429 {
         System.out.println("");
         System.out.println(s1);
         System.out.println(s2);
-        System.out.println(s1.equals(s2.toString()));
+        System.out.println(s1.equals(s2.toString()));*/
 
+        int t = 2;
+        Stack s = new Stack();
+        int[] arrs = ArrayFactory.getArray(10, 50);
+        String s1 = pushs(arrs,s);
+        int i = 0;
+        while( i < t ){
+            i++;
+            s = stackOp(s);
+        }
+        String s2 = pops(s);
+        System.out.println(s1);
+        System.out.println(s2);
     }
 
+    public static  String pushs(int[] arrs,Stack s){
+//        Stack s = new Stack();
+        StringBuilder sb = new StringBuilder();
+        for(int i : arrs){
+            s.push(i);
+            sb.append(i +"-");
+        }
+        return sb.toString();
+    }
+
+    public static String getStackOp(Stack s1){
+        return pops(stackOp(s1));
+    }
+
+    public static String pops(Stack s){
+        StringBuilder sb = new StringBuilder();
+        while (!s.empty()){
+            sb.append(s.pop() +"-");
+        }
+        return sb.toString();
+    }
+
+    public static Stack stackOp(Stack s1){
+        Stack s2 = new Stack();
+        while(!s1.empty()){
+            s2.push(s1.pop());
+        }
+        return s2;
+    }
+
+
+
+    /**
+    * 栈有反序作用，并且具有传递性，如果 栈 栈 右能顺序
+     **/
 
     public static void back(TreeNode node,StringBuilder sb){
         if(node == null){
